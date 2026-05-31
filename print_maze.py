5f3b85fa14b0fa2db167191bb8310463771b5287
Rashed Alrwajbeh <rashedalrwajbeh@gmail.com>
2026-06-01 01:35:30 +0300

    remove make_output function from print_maze.py because i put it in Generator.py

--- print_maze.py
+++ print_maze.py
@@ -102,21 +102,3 @@ def print_grid(
     print()
 
     print(RESET, end="")
-
-
-def make_output(cells: list[cell], ans: list[str], start: tuple[int, int],
-                end: tuple[int, int], cols: int, rows: int,
-                out_file: str) -> None:
-    with open(out_file, "w") as file:
-        for i in range(rows):
-            for j in range(cols):
-                idx = i * cols + j
-                file.write(cells[idx].change_to_Hex())
-            file.write("\n")
-        file.write("\n")
-
-        file.write(f"{start[0]}, {start[1]}\n")
-        file.write(f"{end[0]}, {end[1]}\n")
-        for ii in ans:
-            file.write(ii)
-        file.write("\n")
